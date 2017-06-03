e72e7aaea2fbb39f323dea15ccc115a6781819aa
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 22:38:16 +0530

    AGAIN

--- app/src/index.php
+++ app/src/index.php
@@ -21,7 +21,7 @@
         </div>
       </row>
       <row>
-        <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "content">
+        <div class = "content col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <?php include "signup.php";?>
         </div>
       </row>
