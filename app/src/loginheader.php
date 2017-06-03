b7af01899eef528e450809dcc72bed234f1b7282
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 17:59:31 +0530

    Finally

--- app/src/loginheader.php
+++ app/src/loginheader.php
@@ -5,8 +5,8 @@
   </div>
   <div class = "col-sm-6" id = "loginspace">
     <form name = "loginform" id = "loginform">
-      <input type = "text" name = "primarykey" id = "primarykey" placeholder="Email or Mobile"></br>
-      <input type = "password" name = "password" id = "password" placeholder="Password"></br>
+      <input type = "text" name = "primarykey" id = "primarykey" placeholder="Email or Mobile">
+      <input type = "password" name = "password" id = "password" placeholder="Password">
       <button type = "submit" name = "login">Log in</button>
     </form>
   </div>
