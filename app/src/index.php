1c026e078da657a1d9e1496fdbc648f1ebc3fab5
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 22:15:20 +0530

    AGAIN

--- app/src/index.php
+++ app/src/index.php
@@ -21,17 +21,10 @@
         </div>
       </row>
       <row>
-        <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <img class = "img-responsive" src = "/css/sunshine-min.jpg" alt = "Sunshine.JPG">
-          <div class = "container">
-            <row>
-              <div class = "signup col-lg-6 col-md-6 col-sm-6 col-xs-6">
-              </div>
-              <div class = "signup col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                <?php include "signup.php";?>
-              </div>
-            </row>
-          </div>
+        <div class = "signup col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        </div>
+        <div class = "signup col-lg-6 col-md-6 col-sm-6 col-xs-6">
+          <?php include "signup.php";?>
         </div>
       </row>
       <row>
