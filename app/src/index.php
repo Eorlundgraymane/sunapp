b5a252943cc21393d511420516515da778b62eb3
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 20:41:27 +0530

    AGAIN

--- app/src/index.php
+++ app/src/index.php
@@ -21,7 +21,7 @@
         </div>
       </row>
       <row>
-        <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
+        <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6"><img src = "/css/sunshine-min.jpg">
         </div>
         <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
           <?php include "signup.php";?>
