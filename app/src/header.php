b6999204e472eb775a73fc153cbcfde920862bc9
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 15:38:00 +0530

    Sunshine Landing Page June 3rd Commit - 1 3:37PM

--- app/src/header.php
+++ app/src/header.php
@@ -2,7 +2,6 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- jQuery library -->
@@ -10,12 +9,8 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="/css/master.css?v=<?php echo '$time()';?>" media="screen" title="no title">
-    <meta name = "viewport" content = "width=device-width,initial-scale=1">
-    <title>Sunshine | Welcome</title>
   </head>
   <body>
-    <?php include "header.php";?>
-    <?php include "footer.php";?>
+    <div class = ".col-sm-12"><div class = "page-header">Sunshine</div></div>
   </body>
 </html>
