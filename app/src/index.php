a8880f67572cf58ac98c732612e78dd1952b44ab
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-03 20:59:00 +0530

    AGAIN

--- app/src/index.php
+++ app/src/index.php
@@ -20,13 +20,13 @@
           <?php include "loginheader.php";?>
         </div>
       </row>
-      <row id = "maincontent">
+      <div class = "row">
         <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
         </div>
         <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
           <?php include "signup.php";?>
         </div>
-      </row>
+      </div>
       <row>
         <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <?php include "footer.php";?>
