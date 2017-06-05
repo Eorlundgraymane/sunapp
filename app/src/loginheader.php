1c1835c3e2778b461dba76a738023ce78ff6fecc
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 19:03:01 +0530

    OTP OVERLAY SETUP

--- app/src/loginheader.php
+++ app/src/loginheader.php
@@ -1,12 +1,10 @@
-<row>
-  <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename">
-      Sunshine
-  </div>
-  <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "loginspace">
-    <form name = "loginform" id = "loginform">
-      <input type = "text" name = "primarykey" id = "primarykey" placeholder="Email or Mobile">
-      <input type = "password" name = "password" id = "password" placeholder="Password">
-      <button type = "submit" name = "login">Log in</button>
-    </form>
-  </div>
-</row>
+<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename">
+    Sunshine
+</div>
+<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "loginspace">
+  <form name = "loginform" id = "loginform">
+    <input type = "text" name = "primarykey" id = "primarykey" placeholder="Email or Mobile">
+    <input type = "password" name = "password" id = "password" placeholder="Password">
+    <button type = "submit" name = "login">Log in</button>
+  </form>
+</div>
