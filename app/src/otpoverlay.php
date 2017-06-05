724d67ea358e5521d014999462a0ec9c14e136c0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 23:10:27 +0530

    OTP OVERLAY SETUP

--- app/src/otpoverlay.php
+++ app/src/otpoverlay.php
@@ -1,7 +1,7 @@
 <div id = "otpform" class = "otpform">
   <div id = "centerform">
     <form method  = "POST" name = "otpform">
-      <input type = "text" id = "mobile" name = "mobile" placeholder="Mobile"><br>
+      <input type = "text" id = "mobile" name = "remobile" placeholder="Mobile"><br>
       <input type = "text" id = "otp" name = "OTP" placeholder="OTP"><br>
       <button type = "button" id = "otpbutton" onclick = "otpverify()">Verify Mobile</button>
       <button type = "button" id = "resendotp" onclick = "otpresend()">Resend OTP</button><br>
