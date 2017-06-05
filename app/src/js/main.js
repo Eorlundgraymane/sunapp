8345a3aed4a653202fbeb0c39b53e44429987ac1
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 22:19:08 +0530

    OTP OVERLAY SETUP

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -31,7 +31,7 @@ function otpverify(){
   }
   var otp = document.getElementById('otp').value;
   var mobile = document.getElementById('mobile').value;
-  var data = JSON.stringify({"mobile":String(mobile),"otp":String(otp)});
+  var data = JSON.stringify({[mobile]:mobile,[otp]:otp});
   xhr.send(data);
 }
 function resendotp(){
@@ -48,7 +48,7 @@ function resendotp(){
     }
   }
   var remobile = document.getElementById('mobile').value;
-  var data = JSON.stringify({"mobile":String(remobile)});
+  var data = JSON.stringify({[mobile]:remobile});
   xhr.send(data);
 }
 function popalert() {
@@ -70,7 +70,7 @@ var password = document.getElementById("password").value;
 var email = document.getElementById("email").value;
 var uname = fname.concat(" ",lname);
 console.log(uname);
-var data = JSON.stringify({"username":uname+"","email":email+"","password":password+"","mobile":mobile+""});
+var data = JSON.stringify({[username]:uname,[email]:email,[password]:password,[mobile]:mobile});
 xhr.send(data);
 otpoverlaydropdown();
 /*
