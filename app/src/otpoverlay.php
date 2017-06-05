616989a23b259822618ee0e01151eb456a439374
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 20:38:47 +0530

    OTP OVERLAY SETUP

--- app/src/otpoverlay.php
+++ app/src/otpoverlay.php
@@ -1,5 +1,5 @@
-<div id = "otpform" class = "otpform">
-  <form method  = "POST" name = "otpform">
+<div class = "otpform">
+  <form method  = "POST" name = "otpform" id = "otpform">
     <input type = "text" name = "mobile" placeholder="Mobile"><br>
     <input type = "text" name = "OTP" placeholder="OTP"><br>
     <button type = "button" name = "otpbutton">Verify Mobile</button>
