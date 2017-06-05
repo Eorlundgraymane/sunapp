2cca9edf472609f4abc2d6908c36e9197dba6ec6
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 17:53:48 +0530

    Added Click script in JS

--- app/src/signup.php
+++ app/src/signup.php
@@ -143,6 +143,6 @@
   <!--<input type = "date" name = "dob" id = "dob"><br><br> Rendered Obsolete-->
   <input type = "password" name = "password" placeholder="Password"><br>
   <input type = "password" name = "confpass" placeholder="Retype Password"><br>
-  <button name = "signupbutton">Sign Up</button>
+  <button name = "signupbuttn">Sign Up</button>
   <a href = "/">Already received OTP</a>
 </form>
