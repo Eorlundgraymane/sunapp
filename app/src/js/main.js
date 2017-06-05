af830f51d3ac63db83098e717667e78b10d76736
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 20:35:19 +0530

    OTP OVERLAY SETUP

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,13 +1,17 @@
 function otpoverlaydropdown(){
   var otpform =  document.getElementById('otpform');
+  var signupdiv =  document.getElementById('signupdiv');
   otpform.style.opacity = "1";
   otpform.style.zIndex = "2";
+  signupdiv.style.opacity = "0.5";
 
 }
 function otpoverlayslideup(){
   var otpform =  document.getElementById('otpform');
+  var signupdiv =  document.getElementById('signupdiv');
   otpform.style.opacity = "0";
   otpform.style.zIndex = "-2";
+  signupdiv.style.opacity = "0.5";
 }
 
 function popalert() {
