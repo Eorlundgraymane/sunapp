0e948bbdb5fd2ec0a94ab58380cb798f9bcf9e89
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 20:50:02 +0530

    OTP OVERLAY SETUP

--- app/src/otpoverlay.php
+++ app/src/otpoverlay.php
@@ -1,8 +1,10 @@
 <div id = "otpform" class = "otpform">
-  <form method  = "POST" name = "otpform">
-    <input type = "text" name = "mobile" placeholder="Mobile"><br>
-    <input type = "text" name = "OTP" placeholder="OTP"><br>
-    <button type = "button" name = "otpbutton">Verify Mobile</button>
-    <a onclick = "otpoverlayslideup()">Cancel</a>
-  </form>
+  <div id = "centerform">
+    <form method  = "POST" name = "otpform">
+      <input type = "text" name = "mobile" placeholder="Mobile"><br>
+      <input type = "text" name = "OTP" placeholder="OTP"><br>
+      <button type = "button" name = "otpbutton">Verify Mobile</button>
+      <button type = "button" name = "otpcancel" onclick = "otpoverlayslideup()">Cancel</button>
+    </form>
+  </div>
 </div>
