7736867406dbf8c4d0f3f3e9f7fee9dbb4d3c117
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-05 17:29:07 +0530

    Added Click script in JS

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,13 +3,13 @@ xhr = new XMLHttpRequest();
 var url  = "http://auth.vcap.me/signup";
 xhr.open("POST",url,true);
 xhr.setRequestHeader("Content-type","application/json");
-xhr.onreadystatechange = function(){
+/*xhr.onreadystatechange = function(){
   if(xhr.readyState == 4 && xhr.status == 200){
     var json = JSON.parse(xhr.responseText);
     console.log(JSON.stringify(json));
     alert("JSON received");
   }
-};
+};*/
 var data = JSON.stringify({"username":"rkmjstester","email":"hey@mail.com","password":"justanotherpassword","mobile":"7025568897"});
 xhr.send(data);
 }
