8ff5f8ddaf6f9bb4913023967588bdb4087e4831
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-06 12:37:41 +0530

    Temporary Disable of certain Buttons

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -6,8 +6,9 @@ function otpoverlaydropdown(){
   otpform.style.opacity = "1";
   otpform.style.zIndex = "2";
   signupdiv.style.opacity = "0.5";
-  while (otpform.style.height <=250) {
+  while (otpform.style.height <= 250) {
     setTimeout(function(){},100);
+    console.log(otpform.style.height);
     expand();
   }
   function expand(){
