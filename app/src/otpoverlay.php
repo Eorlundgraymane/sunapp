d5be81d30ed8862a3ead0d6ce6a0bb2cb1f60981
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-06 11:05:25 +0530

    Temporary Disable of certain Buttons

--- app/src/otpoverlay.php
+++ app/src/otpoverlay.php
@@ -3,8 +3,8 @@
     <form method  = "POST" name = "otpform">
       <input type = "text" id = "mobile" name = "remobile" placeholder="Mobile"><br>
       <input type = "text" id = "otp" name = "OTP" placeholder="OTP"><br>
-      <button type = "button" id = "otpbutton" onclick = "otpverify()">Verify Mobile</button>
-      <button type = "button" id = "resendotp" onclick = "otpresend()">Resend OTP</button><br>
+      <button disabled = "disabled"  type = "button" id = "otpbutton" onclick = "otpverify()">Verify Mobile</button>
+      <button disabled = "disabled"  type = "button" id = "resendotp" onclick = "otpresend()">Resend OTP</button><br>
       <button type = "button" id = "otpcancel" onclick = "otpoverlayslideup()">Cancel</button>
     </form>
   </div>
