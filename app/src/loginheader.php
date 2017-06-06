cbd2205a73a97170b44982be21174447c848d9ac
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-06 11:12:12 +0530

    Temporary Disable of certain Buttons

--- app/src/loginheader.php
+++ app/src/loginheader.php
@@ -5,6 +5,6 @@
   <form name = "loginform" id = "loginform">
     <input type = "text" name = "primarykey" id = "primarykey" placeholder="Email or Mobile">
     <input type = "password" name = "password" id = "password" placeholder="Password">
-    <button type = "submit" name = "login">Log in</button>
+    <button type = "submit" id = "loginbutton" name = "login">Log in</button>
   </form>
 </div>
