3b8b9126042d42b8ed89582ef56acfa2f48571c1
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-09 12:37:19 +0530

    Started Development of User Home Page

---
+++ app/src/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'headprereqs.php';?>
+    <title>Sunshine | Home</title>
+  </head>
+  <body class = "outer-container">
+    <row>
+      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id  = "siteheader">
+        <?php include 'header.php';?>
+      </div>
+    </row>
+    <row>
+      <?php include 'footer.php';?>
+    </row>
+  </body>
+</html>
--- app/src/index.php
+++ app/src/index.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="/js/main.js?ver=<?php echo time();?>" type="text/javascript"></script>
-    <link rel="stylesheet" href="/css/master.css?ver=<?php echo time();?>" media="screen" title="no title">
-    <meta name = "viewport" content = "width=device-width,initial-scale=1">
+    <?php include 'headprereqs.php';?>
     <title>Sunshine | Welcome</title>
   </head>
     <body class = "outer-container">
