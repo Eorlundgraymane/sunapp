7ad39bee48381f08429f1f466eefe25430670e49
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 21:43:23 +0530

    Development of Home Page started

--- app/src/header.php
+++ app/src/header.php
@@ -6,7 +6,14 @@
   <body>
     <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitename">
         Sunshine<br>
-        <?php echo' Checking to see if the post values reached here safely '.$_POST["primarykey"].'is the primaey key and '.$_POST["password"].' is the password';?>
+        <?php
+        if(isset($_POST['primarykey'])&& isset($_POST['password'])){
+        echo' Checking to see if the post values reached here safely '.$_POST["primarykey"].'is the primaey key and '.$_POST["password"].' is the password';
+        }
+        else {
+          echo' Checking to see if the post values reached here safely ...and nope, it seem\'s the\'re dead';
+        }
+        ?>
     </div>
   </body>
 </html>
