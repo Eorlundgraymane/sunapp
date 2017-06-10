29a20c7736e050e12b5d292cdc8cc4237f4c6673
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 21:57:39 +0530

    Development of Home Page started

--- app/src/header.php
+++ app/src/header.php
@@ -14,7 +14,7 @@
           echo' Checking to see if the post values reached here safely ...and nope, it seem\'s the\'re dead';
         }
         ?>
-        Click <a href = "#" onclick = "userlogout(<?php echo $_POST["primarykey"];?>);">here</a> to logout safely so that you don't cause any problems to my programming
+        Click <button type = "button" onclick = "userlogout(<?php echo $_POST["primarykey"];?>);">here</button> to logout safely so that you don't cause any problems to my programming
     </div>
   </body>
 </html>
