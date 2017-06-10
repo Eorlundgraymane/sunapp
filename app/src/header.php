a6285c706d5ec69cab74d2c7243b34cd44f7083c
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 21:42:03 +0530

    Development of Home Page started

--- app/src/header.php
+++ app/src/header.php
@@ -6,7 +6,7 @@
   <body>
     <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitename">
         Sunshine<br>
-        <?php echo' Checking to see if the post values reached here safely '+$_POST["primarykey"]+'is the primaey key and '+$_POST["password"]+' is the password';?>
+        <?php echo' Checking to see if the post values reached here safely '.$_POST["primarykey"].'is the primaey key and '.$_POST["password"].' is the password';?>
     </div>
   </body>
 </html>
