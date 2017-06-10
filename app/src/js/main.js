a37c54b3ea63c0f0dd25207a3b1c7e754d999844
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 00:23:16 +0530

    Home Page

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -106,7 +106,7 @@ function comparepass(){
   var pass = document.getElementById('pass').value;
   var confpass = document.getElementById('confpass').value;
   console.log(pass);
-  if(pass == null){
+  if(pass == null || pass == ""){
     document.getElementById('passlabel').classList.remove("passshow");
     document.getElementById('passlabel').classList.add("passlabel");
     document.getElementById('passlabel').innerHTML = "Password Empty";
