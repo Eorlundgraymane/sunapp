327e58a387e2564415498ed76cdb883f22422dbf
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 15:56:56 +0530

    Minor changes to Login technique

--- app/src/signup.php
+++ app/src/signup.php
@@ -2,9 +2,7 @@
 <form methhod = "POST" name = "signup" id = "signup">
   <input type = "text" name = "fname" id = "fname" placeholder="First Name" onkeypress="updateusername()">
   <input type = "text" name = "lname" id  = "lname" placeholder="Last Name" onkeypress="updateusername()"><br>
-  <iframe>
-    <input type = "text" name = "uname" id = "uname" placeholder="Username" disabled="true"><br>
-  </iframe>
+  <input type = "text" name = "uname" id = "uname" placeholder="Username" disabled="true"><br>  
   <input type = "text" name = "email" id = "email" placeholder="Email ID"><br>
   <input type = "text" name = "mobile"id = "mob" placeholder="Mobile"><br>
   <select name = "day" id = "day">
