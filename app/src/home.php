6e1e5869671f02514f8e691ef121e38746796641
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 21:15:01 +0530

    Development of Home Page started

--- app/src/home.php
+++ app/src/home.php
@@ -3,7 +3,8 @@
   if(isset($_POST['primarykey'])&& isset($_POST['password'])){
     setcookie('mobilenumber',$_POST['primarykey'],time()+60*60*24*365,'/','sunshine.washtub66.hasura-app.io');
     setcookie('password',$_POST['password']);
-  }
+    echo '<script>alert("Cookie mobile number = '+$_COOKIE["mobilenumber"]+'is set");';
+    }
  ?>
 <html>
   <head>
