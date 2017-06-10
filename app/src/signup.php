12972adaa9b9cca29cf80f0a661cdee77963e58e
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 17:45:51 +0530

    Added Password Match feature and changed look of input fields

--- app/src/signup.php
+++ app/src/signup.php
@@ -6,6 +6,7 @@
   <input type = "text" name = "email" id = "email" placeholder="Email ID"><br>
   <input type = "text" name = "mobile"id = "mob" placeholder="Mobile"><br>
   <select name = "day" id = "day">
+    <option value = "00">Day</option>
     <option value = "01">1</option>
     <option value = "02">2</option>
     <option value = "03">3</option>
@@ -37,9 +38,9 @@
     <option value = "29">29</option>
     <option value = "30">30</option>
     <option value = "31">31</option>
-    <option value = "00">Day</option>
   </select>
   <select name = "month" id = "month">
+    <option value = "00">Month</option>
     <option value = "01">January</option>
     <option value = "02">February</option>
     <option value = "03">March</option>
@@ -52,71 +53,73 @@
     <option value = "10">October</option>
     <option value = "11">November</option>
     <option value = "12">December</option>
-    <option value = "00">Month</option>
   </select>
   <select name = "year" id = "year">
-    <option value = "1999">1999</option>
-    <option value = "1998">1998</option>
-    <option value = "1997">1997</option>
-    <option value = "1996">1996</option>
-    <option value = "1995">1995</option>
-    <option value = "1994">1994</option>
-    <option value = "1993">1993</option>
-    <option value = "1992">1992</option>
-    <option value = "1991">1991</option>
-    <option value = "1990">1990</option>
-    <option value = "1989">1989</option>
-    <option value = "1988">1988</option>
-    <option value = "1987">1987</option>
-    <option value = "1986">1986</option>
-    <option value = "1985">1985</option>
-    <option value = "1984">1984</option>
-    <option value = "1983">1983</option>
-    <option value = "1982">1982</option>
-    <option value = "1981">1981</option>
-    <option value = "1980">1980</option>
-    <option value = "1979">1979</option>
-    <option value = "1978">1978</option>
-    <option value = "1977">1977</option>
-    <option value = "1976">1976</option>
-    <option value = "1975">1975</option>
-    <option value = "1974">1974</option>
-    <option value = "1973">1973</option>
-    <option value = "1972">1972</option>
-    <option value = "1971">1971</option>
-    <option value = "1970">1970</option>
-    <option value = "1969">1969</option>
-    <option value = "1968">1968</option>
-    <option value = "1967">1967</option>
-    <option value = "1966">1966</option>
-    <option value = "1965">1965</option>
-    <option value = "1964">1964</option>
-    <option value = "1963">1963</option>
-    <option value = "1962">1962</option>
-    <option value = "1961">1961</option>
-    <option value = "1960">1960</option>
-    <option value = "1959">1959</option>
-    <option value = "1958">1958</option>
-    <option value = "1957">1957</option>
-    <option value = "1956">1956</option>
-    <option value = "1955">1955</option>
-    <option value = "1954">1954</option>
-    <option value = "1953">1953</option>
-    <option value = "1952">1952</option>
-    <option value = "1951">1951</option>
-    <option value = "1950">1950</option>
-    <option value = "1949">1949</option>
-    <option value = "1948">1948</option>
-    <option value = "1947">1947</option>
-    <option value = "1946">1946</option>
-    <option value = "1945">1945</option>
-    <option value = "1944">1944</option>
-    <option value = "1943">1943</option>
-    <option value = "1942">1942</option>
-    <option value = "1941">1941</option>
-    <option value = "1940">1940</option>
     <option value = "0000">Year</option>
-  </select><br>
+    <option value = "1937">1937</option>
+    <option value = "1938">1938</option>
+    <option value = "1939">1939</option>
+    <option value = "1940">1940</option>
+    <option value = "1941">1941</option>
+    <option value = "1942">1942</option>
+    <option value = "1943">1943</option>
+    <option value = "1944">1944</option>
+    <option value = "1945">1945</option>
+    <option value = "1946">1946</option>
+    <option value = "1947">1947</option>
+    <option value = "1948">1948</option>
+    <option value = "1949">1949</option>
+    <option value = "1950">1950</option>
+    <option value = "1951">1951</option>
+    <option value = "1952">1952</option>
+    <option value = "1953">1953</option>
+    <option value = "1954">1954</option>
+    <option value = "1955">1955</option>
+    <option value = "1956">1956</option>
+    <option value = "1957">1957</option>
+    <option value = "1958">1958</option>
+    <option value = "1959">1959</option>
+    <option value = "1960">1960</option>
+    <option value = "1961">1961</option>
+    <option value = "1962">1962</option>
+    <option value = "1963">1963</option>
+    <option value = "1964">1964</option>
+    <option value = "1965">1965</option>
+    <option value = "1966">1966</option>
+    <option value = "1967">1967</option>
+    <option value = "1968">1968</option>
+    <option value = "1969">1969</option>
+    <option value = "1970">1970</option>
+    <option value = "1971">1971</option>
+    <option value = "1972">1972</option>
+    <option value = "1973">1973</option>
+    <option value = "1974">1974</option>
+    <option value = "1975">1975</option>
+    <option value = "1976">1976</option>
+    <option value = "1977">1977</option>
+    <option value = "1978">1978</option>
+    <option value = "1979">1979</option>
+    <option value = "1980">1980</option>
+    <option value = "1981">1981</option>
+    <option value = "1982">1982</option>
+    <option value = "1983">1983</option>
+    <option value = "1984">1984</option>
+    <option value = "1985">1985</option>
+    <option value = "1986">1986</option>
+    <option value = "1987">1987</option>
+    <option value = "1988">1988</option>
+    <option value = "1989">1989</option>
+    <option value = "1990">1990</option>
+    <option value = "1991">1991</option>
+    <option value = "1992">1992</option>
+    <option value = "1993">1993</option>
+    <option value = "1994">1994</option>
+    <option value = "1995">1995</option>
+    <option value = "1996">1996</option>
+    <option value = "1997">1997</option>
+    <option value = "1998">1998</option>
+    <option value = "1999">1999</option>
+		</select><br>
   <!--<input type = "date" name = "dob" id = "dob"><br><br> Rendered Obsolete-->
   <input type = "password" name = "password" id = "pass" placeholder="Password"><br>
   <input type = "password" name = "confpass" id = "confpass" placeholder="Retype Password" onkeypress() ="comparepass();" onkeyup="comparepass();">
