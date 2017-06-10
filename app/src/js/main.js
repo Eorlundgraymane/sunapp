60645b19be081e2d3c8b46f03d111563dd2c76e2
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-10 19:13:47 +0530

    Added Password Match feature and changed look of input fields

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -19,7 +19,7 @@ function checkfeb(){
 }
 function checkleap()
 {
-  if(Boolean((Boolean(document.getElementById('month').value == 02))&&(Boolean(document.getElementById('day').value >28)))){
+  if(Boolean((Boolean(document.getElementById('month').value == 02))&&(Boolean(Boolean(document.getElementById('day').value >28) && (Boolean(document.getElementById('day').value <30)))))){
       for(var i = 1;i<=64;i++)
     {
       if(Boolean((document.getElementById('year').options[i].value % 4)==0))
