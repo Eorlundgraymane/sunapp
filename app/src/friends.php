48919f2c092b4d43a43a5426851dd5fa3c46518c
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 12:13:47 +0530

    Home Page

--- app/src/friends.php
+++ app/src/friends.php
@@ -1,10 +1,8 @@
 <nav>
-    <button type = "button" class = "btn" data-toggle = "collapse" data-target = "friendslist">
+    <button type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
       <h5>Friend's List</h5>
     </button>
-    <div  class = "collapse" id = "friendslist">
-      <ul >
+      <ul class = "collapse" id = "friendslist">
         <li>Test friend</li>
       </ul>
-    </div>
 </nav>
