bb268db955c2bc6666da7fbb79276e8113ad304e
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 15:40:30 +0530

    Home Page

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -42,7 +42,7 @@ function getfriendslist(){
           if(fxhr.readyState == 4 && fxhr.status == 200){
             var fjson = JSON.parse(fxhr.responseText);
             console.log(JSON.stringify(fjson[0].fname));
-            var friendname = JSON.stringify(fjson[0].fname);
+            var friendname = fjson[0].fname;
             document.getElementById('friendslist').innerHTML += '<li><figure  id = "friend"><img class = "friendimg img-rounded" alt = "Friend\'s Image" src = "css/friendsprite.jpg"><figcaption>'+friendname+'</figcaption></figure></li>';
           }
           else if(fxhr.readyState === 4){
