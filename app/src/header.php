d1bd409d985b289fe0f3aa1cbe053fd25c6f608e
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 11:41:03 +0530

    Home Page

--- app/src/header.php
+++ app/src/header.php
@@ -2,14 +2,12 @@
   Sunshine</h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
-  <div id  = "sessionbutton">
 <?php
   if(isset($_POST['primarykey'])&& isset($_POST['password'])){
-  echo' <button onclick = "changebanner();" type = "button">Change Banner</button><button type = "button" onclick = "userlogout('.$_POST["primarykey"].');">Log Out</button>';
+  echo' <button  class  = "sessionbutton" onclick = "changebanner();" type = "button">Change Banner</button><button class  = "sessionbutton" type = "button" onclick = "userlogout('.$_POST["primarykey"].');">Log Out</button>';
   }
   else {
     echo' You\'re not Logged In and don\'t have permission to access this page. Please <button onclick = "gohome();" type = "button">Click Here</button> to go back to the Welcome Page';
   }
 ?>
-</div>
 </div>
