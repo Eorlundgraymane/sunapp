992c398eee5427449b0f05cea2199995b755acd0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 11:28:32 +0530

    Home Page

--- app/src/header.php
+++ app/src/header.php
@@ -1,7 +1,7 @@
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><h1 id = "siteh1">
   Sunshine</h1>
 </div>
-<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6">
+<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
 <?php
   if(isset($_POST['primarykey'])&& isset($_POST['password'])){
   echo' <button onclick = "changebanner();" type = "button">Change Banner</button><button type = "button" onclick = "userlogout('.$_POST["primarykey"].');">Log Out</button>';
