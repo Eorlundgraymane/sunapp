637a383899874c1aed1221cd5468857b6bda52f4
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 11:52:45 +0530

    Home Page

--- app/src/home.php
+++ app/src/home.php
@@ -14,7 +14,7 @@
   </head>
   <body class = "outer-container">
     <row>
-      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "siteheader">
         <?php include 'header.php';?>
       </div>
     </row>
