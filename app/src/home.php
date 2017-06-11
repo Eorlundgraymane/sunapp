432cf13245bfa5067322254b0c5147d6e8afe833
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-11 11:54:35 +0530

    Home Page

--- app/src/home.php
+++ app/src/home.php
@@ -19,22 +19,24 @@
       </div>
     </row>
     <row>
-      <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "profilepic">
-        <!--<button id = "picbutton" type = "button" onclick="getpiclink();">Click to load image</button>-->
-        <img id = "profileimage" class = "profileimage img-responsive img-rounded" alt = "No Image Uploaded">
-        <script>getpiclink();</script>
-      </div>
-      <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
-        <table id = "shinetable">
-          <tr>
-            <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
-          </tr>
-          <tr>
-            <td><img class = "img-responsive" alt ="Friend Icon"></td><td><img class = "img-responsive" alt ="Earth Icon"></td><td><img class = "img-responsive" alt ="Charity Icon"></td><td><img class = "img-responsive" alt ="Health Icon"></td><td><img class = "img-responsive" alt ="Family Icon"></td>
-          <tr>
-            <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
-          </tr>
-        </table>
+      <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "profilearea">
+        <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "profilepic">
+          <!--<button id = "picbutton" type = "button" onclick="getpiclink();">Click to load image</button>-->
+          <img id = "profileimage" class = "profileimage img-responsive img-rounded" alt = "No Image Uploaded">
+          <script>getpiclink();</script>
+        </div>
+        <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
+          <table id = "shinetable">
+            <tr>
+              <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+            </tr>
+            <tr>
+              <td><img class = "img-responsive" alt ="Friend Icon"></td><td><img class = "img-responsive" alt ="Earth Icon"></td><td><img class = "img-responsive" alt ="Charity Icon"></td><td><img class = "img-responsive" alt ="Health Icon"></td><td><img class = "img-responsive" alt ="Family Icon"></td>
+            <tr>
+              <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
+            </tr>
+          </table>
+        </div>
       </div>
     </row>
     <row>
