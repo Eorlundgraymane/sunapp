fb08048962ced54b90c315c0c5c0f7f3bf00e8a0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 15:02:51 +0530

    Deving Shine Table

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,25 +1,24 @@
-
-  <button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetablediv">
-    Your Shine Table
-  </button>
-  <div id = "shinetablediv" class = "collapse">
-    <table id = "shinetable" class = "table table-responsive">
-     <tr>
-       <td>0</td>
-       <td>0</td>
-       <td>0</td>
-       <td>0</td>
-       <td>0</td>
-     </tr>
-     <tr>
-       <td><img class = "shineicon img-responsive" src  = "css/generated/friend.svg" alt ="Friend Icon"></td>
-       <td><img class = "shineicon img-responsive" src  = "css/generated/earth.svg" alt ="Earth Icon"></td>
-       <td><img class = "shineicon img-responsive" src  = "css/generated/charity.svg" alt ="Charity Icon"></td>
-       <td><img class = "shineicon img-responsive" src  = "css/generated/health.svg"  alt ="Health Icon"></td>
-       <td><img class = "shineicon img-responsive" src  = "css/generated/family.svg" alt ="Family Icon"></td>
-     </tr>
-     <tr>
-       <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
-     </tr>
-   </table>
- </div>
+<button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetablediv">
+  Your Shine Table
+</button>
+<div id = "shinetablediv" class = "collapse">
+  <table id = "shinetable" class = "table table-responsive">
+   <tr>
+     <td>0</td>
+     <td>0</td>
+     <td>0</td>
+     <td>0</td>
+     <td>0</td>
+   </tr>
+   <tr>
+     <td><img class = "shineicon img-responsive" src  = "css/generated/friend.svg" alt ="Friend Icon"></td>
+     <td><img class = "shineicon img-responsive" src  = "css/generated/earth.svg" alt ="Earth Icon"></td>
+     <td><img class = "shineicon img-responsive" src  = "css/generated/charity.svg" alt ="Charity Icon"></td>
+     <td><img class = "shineicon img-responsive" src  = "css/generated/health.svg"  alt ="Health Icon"></td>
+     <td><img class = "shineicon img-responsive" src  = "css/generated/family.svg" alt ="Family Icon"></td>
+   </tr>
+   <tr>
+     <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
+   </tr>
+ </table>
+</div>
