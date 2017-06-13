0814ae6a2a4f17b1b018f5a1e731b5b4e5a27cc8
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 18:11:32 +0530

    Deving Shine Table

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -765,6 +765,7 @@ function addfriend(id){
   data["args"].objects = [{"user_id":hasura_id,"friend_id":id}];
   xhr.open("POST",url,true);
   xhr.setRequestHeader("Content-type","application/json");
+  xhr.withCredentials = true;
   xhr.onreadystatechange = function(){
     if(xhr.readyState == 4 && xhr.status == 200){
       var response = JSON.parse(xhr.responseText);
