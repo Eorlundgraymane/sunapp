bc39abae7a510673546156e995394f586c20b8a0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 14:17:32 +0530

    Deving Shine Table

--- app/src/resthome.php
+++ app/src/resthome.php
@@ -9,6 +9,7 @@
  <script>getpiclink();</script>
 </div>
 <?php include 'shinetable.php';?>
+</div>
 </row>
 <row>
 <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "contentarea">
--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -5,24 +5,18 @@
   <div id = "shinetablediv" class = "collapse">
     <table id = "shinetable">
      <tr>
-       <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+       <td>0</td>
+       <td>0</td>
+       <td>0</td>
+       <td>0</td>
+       <td>0</td>
      </tr>
      <tr>
-       <td>
-         <img class = "shineicon img-responsive" src  = "css/generated/friend.svg" alt ="Friend Icon">
-       </td>
-       <td>
-         <img class = "shineicon img-responsive" src  = "css/generated/earth.svg" alt ="Earth Icon">
-       </td>
-       <td>
-         <img class = "shineicon img-responsive" src  = "css/generated/charity.svg" alt ="Charity Icon">
-       </td>
-       <td>
-         <img class = "shineicon img-responsive" src  = "css/generated/health.svg"  alt ="Health Icon">
-       </td>
-       <td>
-         <img class = "shineicon img-responsive" src  = "css/generated/family.svg" alt ="Family Icon">
-       </td>
+       <td><img class = "shineicon img-responsive" src  = "css/generated/friend.svg" alt ="Friend Icon"></td>
+       <td><img class = "shineicon img-responsive" src  = "css/generated/earth.svg" alt ="Earth Icon"></td>
+       <td><img class = "shineicon img-responsive" src  = "css/generated/charity.svg" alt ="Charity Icon"></td>
+       <td><img class = "shineicon img-responsive" src  = "css/generated/health.svg"  alt ="Health Icon"></td>
+       <td><img class = "shineicon img-responsive" src  = "css/generated/family.svg" alt ="Family Icon"></td>
      </tr>
      <tr>
        <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
