5251f69825a56c2f05517bbd726a0210ca3a22db
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 13:13:39 +0530

    Getting Friend's List

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,5 +1,5 @@
 <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
- <table id = "shinetable" class = "collpase">
+ <table id = "shinetable" class = "collapse">
    <tr>
      <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
    </tr>
