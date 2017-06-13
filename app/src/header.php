2c6469b54822d4c44857c984b2f1fa9657ee4751
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 15:25:45 +0530

    Deving Shine Table

--- app/src/header.php
+++ app/src/header.php
@@ -4,3 +4,5 @@
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
  <button id = "changebanner" class  = "btn sessionbutton" onclick = "changebanner();" type = "button">Change Banner</button><button id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout('<?php echo $_POST["primarykey"];?>');">Log Out</button>
 </div>
+</div>
+</row>
--- app/src/resthome.php
+++ app/src/resthome.php
@@ -1,6 +1,3 @@
-
-</div>
-</row>
 <row>
 <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "profilearea">
 <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "profilepic">
