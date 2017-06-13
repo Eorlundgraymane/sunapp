4d0f2f27eaa7442bdaab35f07c7af09cefd13213
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 15:14:48 +0530

    Deving Shine Table

--- app/src/loginheader.php
+++ app/src/loginheader.php
@@ -2,15 +2,9 @@
     Sunshine
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "loginspace">
-  <form class = "form-inline" method="POST" action = "/home.php" name = "loginform" id = "loginform">
-    <div class = "input-group">
-      <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i><span>
-      <input type = "text" name = "primarykey" id = "primarykey" placeholder="Mobile">
-    </div>
-    <div class = "input-group">
-      <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i><span>
+  <form method="POST" action = "/home.php" name = "loginform" id = "loginform">
+    <input type = "text" name = "primarykey" id = "primarykey" placeholder="Mobile">
     <input type = "password" name = "password" id = "password" placeholder="Password">
-  </div>
     <button class = "btn" type = "button" id = "loginbutton" name = "login" onclick="userlogin();">Log in</button>
   </form>
 </div>
--- app/src/otpoverlay.php
+++ app/src/otpoverlay.php
@@ -1,14 +1,8 @@
 <div id = "otpform" class = "otpform">
   <div id = "centerform">
-    <form class = "form-horizontal" method  = "POST" name = "otpform">
-      <div class = "input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i><span>
+    <form method  = "POST" name = "otpform">
       <input type = "text" id = "mobile" name = "remobile" placeholder="Mobile"><br>
-    </div>
-      <div class = "input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i><span>
       <input type = "text" id = "otp" name = "OTP" placeholder="OTP"><br>
-    </div>
       <button class = "btn" type = "button" id = "otpbutton" onclick = "otpverify()">Verify Mobile</button>
       <button class = "btn" type = "button" id = "resendotp" onclick = "otpresend()">Resend OTP</button><br>
       <button class = "btn" type = "button" id = "otpcancel" onclick = "otpoverlayslideup()">Cancel</button>
