12e2fbc71100175ca0b15f65d1833ea29f6f4cc0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 15:38:02 +0530

    Deving Shine Table

--- app/src/resthome.php
+++ app/src/resthome.php
@@ -15,7 +15,7 @@
 <row>
   <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "contentarea">
     <row>
-      <div class = "col-lg-4 col-md-4 col-sm-4 col-xs-3" id = "friendspane">
+      <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "friendspane">
         <?php include 'friends.php';?>
       </div>
       <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
