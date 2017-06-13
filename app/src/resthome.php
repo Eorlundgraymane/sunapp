3c610dac22c614fdeff112f5b248f5dc19ced371
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 15:37:30 +0530

    Deving Shine Table

--- app/src/resthome.php
+++ app/src/resthome.php
@@ -1,25 +1,29 @@
 <row>
 <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "profilearea">
-<div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "profilepic">
- <!--<button id = "picbutton" type = "button" onclick="getpiclink();">Click to load image</button>-->
- <img id = "profileimage" class = "profileimage img-responsive img-rounded" alt = "No Image Uploaded">
- <script>getpiclink();</script>
-</div>
-<div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
-<?php include 'shinetable.php';?>
-</div>
+  <row>
+    <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "profilepic">
+     <!--<button id = "picbutton" type = "button" onclick="getpiclink();">Click to load image</button>-->
+     <img id = "profileimage" class = "profileimage img-responsive img-rounded" alt = "No Image Uploaded">
+     <script>getpiclink();</script>
+    </div>
+    <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
+      <?php include 'shinetable.php';?>
+    </div>
+  </row>
 </div>
 </row>
 <row>
-<div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "contentarea">
-<div class = "col-lg-4 col-md-4 col-sm-4 col-xs-3" id = "friendspane">
- <?php include 'friends.php';?>
-</div>
-<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
-</div>
-<div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "postpane">
-</div>
-</div>
+  <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "contentarea">
+    <row>
+      <div class = "col-lg-4 col-md-4 col-sm-4 col-xs-3" id = "friendspane">
+        <?php include 'friends.php';?>
+      </div>
+      <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
+      </div>
+      <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "postpane">
+      </div>
+    </row>
+  </div>
 </row>
 <row>
 <?php include 'footer.php';?>
