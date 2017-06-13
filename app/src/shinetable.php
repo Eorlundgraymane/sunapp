96a5c4515ab87322620615206e4f98a38ab849c3
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 13:20:19 +0530

    Getting Friend's List

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,16 +1,18 @@
 <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
-  <button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetable">
+  <button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetablediv">
     Your Shine Table
   </button>
- <table id = "shinetable" class = "collapse">
-   <tr>
-     <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
-   </tr>
-   <tr>
-     <td><img class = "img-responsive" alt ="Friend Icon"></td><td><img class = "img-responsive" alt ="Earth Icon"></td><td><img class = "img-responsive" alt ="Charity Icon"></td><td><img class = "img-responsive" alt ="Health Icon"></td><td><img class = "img-responsive" alt ="Family Icon"></td>
-   <tr>
-     <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
-   </tr>
- </table>
+  <div id = "shinetablediv" class = "collapse">
+    <table id = "shinetable">
+     <tr>
+       <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+     </tr>
+     <tr>
+       <td><img class = "img-responsive" alt ="Friend Icon"></td><td><img class = "img-responsive" alt ="Earth Icon"></td><td><img class = "img-responsive" alt ="Charity Icon"></td><td><img class = "img-responsive" alt ="Health Icon"></td><td><img class = "img-responsive" alt ="Family Icon"></td>
+     <tr>
+       <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
+     </tr>
+   </table>
+ </div>
 </div>
 </div>
