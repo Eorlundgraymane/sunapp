dd71cda518b4b2d9f90bd35c77d3b6ffc4c3fd59
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 13:14:57 +0530

    Getting Friend's List

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,4 +1,7 @@
 <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
+  <button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetable">
+    Your Shine Table
+  </button>
  <table id = "shinetable" class = "collapse">
    <tr>
      <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
@@ -10,7 +13,4 @@
    </tr>
  </table>
 </div>
-<button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetable">
-  Your Shine Table
-</button>
 </div>
