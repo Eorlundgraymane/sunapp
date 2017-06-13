6e98afcedd7cb6eb97c1ce36ef3d492ce92945d0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 13:12:09 +0530

    Getting Friend's List

---
+++ app/src/shinetable.php
@@ -0,0 +1,16 @@
+<div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shine">
+ <table id = "shinetable" class = "collpase">
+   <tr>
+     <td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>
+   </tr>
+   <tr>
+     <td><img class = "img-responsive" alt ="Friend Icon"></td><td><img class = "img-responsive" alt ="Earth Icon"></td><td><img class = "img-responsive" alt ="Charity Icon"></td><td><img class = "img-responsive" alt ="Health Icon"></td><td><img class = "img-responsive" alt ="Family Icon"></td>
+   <tr>
+     <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Familyshine</th>
+   </tr>
+ </table>
+</div>
+<button id = "shinetablebutton" type = "button" class = "btn" data-toggle = "collapse" data-target = "#shinetable">
+  Your Shine Table
+</button>
+</div>
