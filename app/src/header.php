fa8060eb19d92e141bf0dc0e6204acc9e704511d
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-13 10:51:45 +0530

    Getting Friend's List

--- app/src/header.php
+++ app/src/header.php
@@ -1,5 +1,5 @@
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><h1 id = "siteh1">
-  Sunshine <img width = '50px' height = '50px' src = 'css/loader.gif'></h1>
+  Sunshine</h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
  <button id = "changebanner" class  = "btn sessionbutton" onclick = "changebanner();" type = "button">Change Banner</button><button id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout('<?php echo $_POST["primarykey"];?>');">Log Out</button>
