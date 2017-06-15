826fff9fd5d901a0ee856980bd9e00fff54e91df
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-15 16:10:47 +0530

    Like

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1282,7 +1282,7 @@ var aDay = 24 * 60 * 60 * 1000;
 console.log(time_ago(new Date(Date.now() - aDay)));
 console.log(time_ago(new Date(Date.now() - aDay * 2)));
 
-function like(id,author_id,liker_id){
+function like(id,liker_id){
   var likimg = document.getElementById('img'+id);
   likimg.src = "css/generated/health.svg";
   var data = {};
