0c3d0d1949846a736e5eacf74936d888f3c347b7
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-15 09:56:44 +0530

    Push Post

--- app/src/resthome.php
+++ app/src/resthome.php
@@ -30,19 +30,12 @@
       <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "postpane">
           <form method = "POST" action = "#">
             <div id = "uploadform" class = "form-group">
-              <label for = "title">Title</label>
               <input  type = "text" id = "newposttitle" placeholder = "Title"><br>
-              <label for = "newpostimg">Add Image URL</label>
               <input  type = "text" id = "newpostimg" placeholder = "Image URL"><br>
-              <label for = "newposttext">Post</label>
               <textarea  id = "newposttext" placeholder="Post here..."></textarea><br>
-              <label for = "earth">Earth Shine</label>
               <input type = "number" id = "earth" placeholder = "Earth Shine"><br>
-              <label for = "charity">Charity Shine</label>
               <input type = "number" id = "charity" placeholder = "Charity Shine"><br>
-              <label for = "health">Health Shine</label>
               <input type = "number" id = "health" placeholder = "Health Shine"><br>
-              <label for = "social">Social Shine</label>
               <input type = "number" id = "social" placeholder = "Social Shine"><br>
               <button class = "btn" type = "button" id = "postbutton" onclick = "pushpost();">Post</button>
             </div>
