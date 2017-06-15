b345e9e9829cbb8f271b98f27ae0c7228c8fd139
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-15 09:55:30 +0530

    Push Post

--- app/src/resthome.php
+++ app/src/resthome.php
@@ -31,19 +31,19 @@
           <form method = "POST" action = "#">
             <div id = "uploadform" class = "form-group">
               <label for = "title">Title</label>
-              <input class = "form-control" type = "text" id = "newposttitle" placeholder = "Title"><br>
+              <input  type = "text" id = "newposttitle" placeholder = "Title"><br>
               <label for = "newpostimg">Add Image URL</label>
-              <input class = "form-control" type = "text" id = "newpostimg" placeholder = "Image URL"><br>
+              <input  type = "text" id = "newpostimg" placeholder = "Image URL"><br>
               <label for = "newposttext">Post</label>
-              <textarea class = "form-control" id = "newposttext" placeholder="Post here..."></textarea><br>
+              <textarea  id = "newposttext" placeholder="Post here..."></textarea><br>
               <label for = "earth">Earth Shine</label>
-              <input class = "form-control" type = "number" id = "earth" placeholder = "Earth Shine"><br>
+              <input type = "number" id = "earth" placeholder = "Earth Shine"><br>
               <label for = "charity">Charity Shine</label>
-              <input class = "form-control" type = "number" id = "charity" placeholder = "Charity Shine"><br>
+              <input type = "number" id = "charity" placeholder = "Charity Shine"><br>
               <label for = "health">Health Shine</label>
-              <input class = "form-control" type = "number" id = "health" placeholder = "Health Shine"><br>
+              <input type = "number" id = "health" placeholder = "Health Shine"><br>
               <label for = "social">Social Shine</label>
-              <input class = "form-control" type = "number" id = "social" placeholder = "Social Shine"><br>
+              <input type = "number" id = "social" placeholder = "Social Shine"><br>
               <button class = "btn" type = "button" id = "postbutton" onclick = "pushpost();">Post</button>
             </div>
           </form>
