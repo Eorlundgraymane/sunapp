afb62dfcf4abbaeb584f729963d56de7fc1bde15
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-17 21:58:28 +0530

    Theming Up

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -661,14 +661,14 @@ function sunshinescroll(){
       if(scroller[0].className.split(' ').length > 1){
         var prevclass = scroller[0].className.split(' ')[1];
         scroller[0].classList.add("sprite-"+sprites[sprite]);scroller[0].classList.remove(prevclass);document.getElementById('message').innerHTML = sprites[sprite+1];
-        if(sprite%2 == 0){
+        if(sprite!=0 && sprite%2 == 0){
           document.getElementById('message').classList.toggle('demessage');
         }
 
       }
       else{
         scroller[0].classList.add("sprite-"+sprites[sprite]);document.getElementById('message').innerHTML = sprites[sprite+1];
-        if(sprite%2 == 0){
+        if(sprite!=0 && sprite%2 == 0){
           document.getElementById('message').classList.toggle('demessage');
         }
       }
