2a01c8dd94e6e7a5ecaaae694b84693088fe4985
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-17 13:44:58 +0530

    Post Author Image Sized Up

--- app/src/friendprofile.php
+++ app/src/friendprofile.php
@@ -1,12 +1,18 @@
 
 <script src = "js/main.js" type = "text/javascript"></script>
 <script>
-if(checkCookie("primarykey")){
-  alert("Hello "+getCookie("primarykey")+" !");
+if(checkCookie("primarykey")&& checkCookie("friendid")){
+  alert("Hello "+getCookie("primarykey")+" This is the profile page of "+getCookie("friendid")+"!");
 }
 else{
-  var cookie = prompt("Enter your primary key");
-  setCookie("primarykey",cookie,10);
+  if(!checkCookie("primarykey")){
+    alert("You're not logged in");
+  }
+  else if(!checkCookie("friendid")){
+    alert("You have not chosen a friend");
+  }
+
 }
+
 </script>
 <button type = "button" onclick = "clearCookies()">Clear All Cookies</button>
