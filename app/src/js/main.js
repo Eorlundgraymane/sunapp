90dab1ca69f66cd9c93cfbdbe76ed2764555b870
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-17 11:21:25 +0530

    Post Author Image Sized Up

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -108,6 +108,24 @@ function SHA256(s){
 }
 
 
+var smileys = {
+  ':)': '<img src="css/smileys/happy-9.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com"/>',
+  ':-)': '<img src="css/smilies/happy-9.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com" />',
+  ':D': '<img src="css/smilies/happy-5.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com" />',
+  ':-(': '<img src="css/smilies/sad-1.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com" />',
+  ':-D': '<img src="css/smilies/happy-5.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com"/>',
+  'lol': '<img src="css/smilies/laughing-2.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com"/>',
+  ':-|': '<img src="css/smilies/sceptic.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com"/>',
+  ';-)': '<img src="css/smilies/winking.svg" width = "16px" height = "16px" border="0" alt="" title = "Designed by http://www.flaticon.com/authors/madebyoliver at http://www.flaticon.com"/>',
+};
+
+function smilyMe(msg) {
+  //smiley replace
+  return msg.replace(/(\:\)|\:-\)|\:D|\:-D|\blol\b|\:-\||\:-\(|\;-\))/g, function(all) {
+    return smileys[all] || all;
+  });
+}
+
 
 function appfriendslogin(){
   if(getflag == 1 || (droppeddown == 0 && friendlistflag == 0))
