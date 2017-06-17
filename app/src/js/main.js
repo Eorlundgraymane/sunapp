5b2f0be8089e7f7d1e94de05a0ab4c0a34cce896
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-17 19:28:06 +0530

    Theming Up

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -657,7 +657,7 @@ function sunshinescroll(){
       setTimeout(function(){
       var scroller  = document.getElementsByClassName("sprite");
       var prevclass = scroller[0].className.split(' ')[1];
-      scroller[0].classList.remove(prevclass); scroller[0].innerHTML = sprites[sprite];scroller[0].classList.add("sprite-"+sprites[sprite]);
+      scroller[0].classList.add("sprite-"+sprites[sprite]);scroller[0].classList.remove(prevclass); scroller[0].innerHTML = sprites[sprite];
       sprite++;
       if(sprite < 10){
         sunshinescroll();
