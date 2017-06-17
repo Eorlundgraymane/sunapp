47f8faa3224dbfac05356c3b0a9a5a3ad053926a
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-17 23:46:13 +0530

    Theming Up

--- app/src/friends.php
+++ app/src/friends.php
@@ -1,10 +1,10 @@
 <nav id = "friendsbuttons">
-    <button id = "friendslistbutton" onclick = "appfriendslogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
+    <button title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
       Friend's List
     </button>
       <ul class = "collapse" id = "friendslist">
       </ul><br>
-      <button id = "friendssuggestbutton" onclick = "applogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendssuggest">
+      <button title = "Friend Suggestions" id = "friendssuggestbutton" onclick = "applogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendssuggest">
         <img width = "30px" height = "30px" src = "css/th.jpg">
       </button>
         <ul class = "collapse" id = "friendssuggest">
