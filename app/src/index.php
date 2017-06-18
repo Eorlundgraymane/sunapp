b4beecc060240386601d9954b8bd90eb26846a76
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-18 22:41:00 +0530

    Index page change

--- app/src/index.php
+++ app/src/index.php
@@ -19,9 +19,12 @@
       <row>
         <div class = "regpane col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <div class = "col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <div id = "messages">
             <div class = "sprite nosprite">
               </div>
               <div id = "message" class = "message"></div>
+              <script>sunshinescroll()</script>
+            </div>
           </div>
           <?php include 'otpoverlay.php';?>
           <?php include "signup.php";?>
