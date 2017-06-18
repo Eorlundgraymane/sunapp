d05b69c17f27535c0ec84fa62c4924be276eb799
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-18 22:09:07 +0530

    Index page change

--- app/src/leaderboards.php
+++ app/src/leaderboards.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sunshine | Leaderboard</title>
-    <?php include 'headprereqes.php';?>
+    <?php include 'headprereqs.php';?>
   </head>
   <body >
     <row>
