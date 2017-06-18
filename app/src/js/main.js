4580f7c64e0e18b115768469fdf9e034813aaf40
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 03:54:31 +0530

    Index page change

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -633,7 +633,7 @@ function getfpiclink(id){
   xhr.onreadystatechange = function(){
     console.log('xhr readystatechange');
     if(xhr.readyState == 4 && xhr.status == 200){
-      console.logI(json["proimage"]);
+      console.log(json);
       var json = JSON.parse(xhr.responseText);
           if(checkCookie("primarykey") && !checkCookie("friendid")){
             pullpost(getCookie("friendid"));
