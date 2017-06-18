0a7fc1d7b0f7889e7d261013297fb0225d0a15e0
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 03:25:58 +0530

    Index page change

---
+++ app/src/friendsfriends.php
@@ -0,0 +1,14 @@
+<nav id = "col-xs-3 col-md-3 col-lg-3 col-sm-3 friendsbuttons">
+    <button title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
+      <img src = "css/friendsicon.png" width = "50px" height = "50px">
+    </button>
+      <ul class = "collapse" id = "friendslist">
+      </ul><br>
+        <div id = "messages">
+        <div class = "col-xs-3 col-md-3 col-lg-3 col-sm-3 sprite nosprite">
+        </div><br><br><br><br><br><br>
+          <div id = "message" class = "message"></div>
+          <script>sunshinescroll()</script>
+        </div>
+
+</nav>
