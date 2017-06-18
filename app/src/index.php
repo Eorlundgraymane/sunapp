8e67ba39e587c783cc0c2477f3a1c12c2e0937c3
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 00:06:46 +0530

    Index page change

--- app/src/index.php
+++ app/src/index.php
@@ -19,8 +19,8 @@
       <row>
         <div class = "regpane col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <div class = "col-xs-8 col-sm-8 col-md-8 col-lg-8">
-            <div class = "col-xs-4 col-sm-4 col-md-4 col-lg-4" id = "homemessages">
-            <div class = "sprite nosprite ">
+            <div id = "homemessages">
+            <div class = "sprite nosprite ">              
               </div>
               <div id = "message" class = "message"></div>
               <script>homescroll()</script>
