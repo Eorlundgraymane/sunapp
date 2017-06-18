36e91e39bdd3867e198268451a50d07dbc64edcd
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-18 22:19:33 +0530

    Index page change

--- app/src/leaderboardtitle.php
+++ app/src/leaderboardtitle.php
@@ -1,3 +1,3 @@
 <div id = "trophy" class = "col-sm-12 col-md-12 col-xs-12 col-lg-12">
-  <img width = "100px" height="140px" src = "css/trophy.png">
+  <img width = "100px" height="130px" src = "css/trophy.png">
 </div>
