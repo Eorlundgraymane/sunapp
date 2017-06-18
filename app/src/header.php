f0a78f97ce75f98fe6e05d2084c8413c6e376c3c
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-18 21:37:18 +0530

    Index page change

--- app/src/header.php
+++ app/src/header.php
@@ -2,7 +2,7 @@
   Sunshine</h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
- <button id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "50px" height = "75px"</button>
+ <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "50px" height = "75px"</button>
 </div>
 </div>
 </row>
