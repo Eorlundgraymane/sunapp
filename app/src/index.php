6b13ba3b7316927d2819bf9ac916657dfeb97ec3
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-18 22:34:43 +0530

    Index page change

--- app/src/index.php
+++ app/src/index.php
@@ -18,6 +18,11 @@
       </row>
       <row>
         <div class = "regpane col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class = "col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <div class = "sprite nosprite">
+              </div>
+              <div id = "message" class = "message"></div>
+          </div>
           <?php include 'otpoverlay.php';?>
           <?php include "signup.php";?>
         </div>
