d36a650e10382d7a029d543af401eb6b88b00c97
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 04:14:09 +0530

    Index page change

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -629,8 +629,10 @@ function getfpiclink(id){
           piclink = json[0]["proimage"];
           document.getElementById('profileimage').src = piclink;
           document.getElementById('profileimage').classList.remove('profileimage');
-          document.getElementById('profileimage').classList.add('profileimageappeared');          
+          document.getElementById('profileimage').classList.add('profileimageappeared');
+          if(checkCookie("primarykey") && checkCookie("friendid")){
           pullfposts(getCookie("friendid"));
+          }
         }
     else if(xhr.readyState ==4) {
       alert(JSON.stringify(json));
