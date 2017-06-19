d4edc8185528166e8de0f46306b88f423925cdcd
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 14:02:54 +0530

    Leaderboards

--- app/src/friends.php
+++ app/src/friends.php
@@ -1,10 +1,10 @@
 <nav id = "col-xs-3 col-md-3 col-lg-3 col-sm-3 friendsbuttons">
-    <button title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
+    <button title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button"= data-toggle = "collapse" data-target = "#friendslist">
       <img src = "css/friendsicon.png" width = "50px" height = "50px">
     </button>
       <ul class = "collapse" id = "friendslist">
       </ul><br>
-      <button title = "Friend Suggestions" id = "friendssuggestbutton" onclick = "applogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendssuggest">
+      <button title = "Friend Suggestions" id = "friendssuggestbutton" onclick = "applogin();" type = "button" data-toggle = "collapse" data-target = "#friendssuggest">
         <img width = "50px" height = "50px" src = "css/th.jpg">
       </button>
         <ul class = "collapse" id = "friendssuggest">
