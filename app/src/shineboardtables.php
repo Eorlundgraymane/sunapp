133aab713b6c174a9db8b4f52738bb26cb9d231a
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 22:54:04 +0530

    Debugs

--- app/src/shineboardtables.php
+++ app/src/shineboardtables.php
@@ -1,5 +1,5 @@
 <div class = "col-sm-12 col-xs-12 col-md-12 col-lg-12">
-  <table class = "table shinetable" id = "leads">
+  <table class = "table table-responsive shinetable" id = "leads">
   </table>
 </div>
 <script>loadleaderboards();</script>
