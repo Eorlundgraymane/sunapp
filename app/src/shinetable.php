c04d13ba6a43ebae1c03a835cde951e43bebde3b
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 22:41:37 +0530

    Debugs

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,15 +1,10 @@
-<row>
-    <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shinetoggles">
+
   <button  title = "Shinetable" class = "btn" onclick = "refreshscore();" id = "shinetablebutton" type = "button" data-toggle = "collapse" data-target = "#shinetablediv">
   <img width = "50px" height = "50px" src = "css/table.png">
 </button>
 <button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button">
   <img width = "50px" height = "50px" src = "css/trophy.png">
 </button>
-</div>
-</row>
-<row>
-<div class = "collapse col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shinetablediv">
 <table id = "shinetable" class = "table table-responsive">
   <caption title = "Post activites,get likes, Improve your Shine"id = "shineheading">Shine Table</caption>
  <tr>
@@ -32,5 +27,3 @@
    <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Socialshine</th><th>Shine</th>
  </tr>
 </table>
-</div>
-</row>
