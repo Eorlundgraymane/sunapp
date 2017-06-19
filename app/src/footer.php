0bdeb0846e9f6841550e707d8eb83587d41ba9ad
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 13:16:23 +0530

    Leaderboards

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,3 +1,3 @@
 <row>
-    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Made by PIRAVI 2.0</div>
+    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Powered by Hasura | Made by PIRAVI 2.0</div>
 </row>
--- app/src/shineboardtables.php
+++ app/src/shineboardtables.php
@@ -1,5 +1,5 @@
 <div class = "col-sm-12 col-xs-12 col-md-12 col-lg-12">
-  <table id = "leads">
+  <table class = "shinetable" id = "leads">
   </table>
 </div>
 <script>loadleaderboards();</script>
