a8a245f569360ffadc557501430832fa6cb5cff7
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 05:39:37 +0530

    Index page change

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -339,7 +339,7 @@ function gethasurapushpost(){
 
 
 function gethasurapullpost(){
-  document.getElementById('postpuller').innerHTML = "Pulling latest Posts <img src = css/loader.gif width = \"30px\" height = \"30px\">";
+  document.getElementById('postpuller').style.transofrm = 'rotate(1440deg)';
   var btns = document.getElementsByClassName('btn');
   for(each of btns){
     each.disabled = true;
