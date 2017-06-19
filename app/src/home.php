a0b33bd5a47caa9e51681fcb7e915fe6ab8ff454
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 14:02:18 +0530

    Leaderboards

--- app/src/home.php
+++ app/src/home.php
@@ -70,7 +70,7 @@
                        <input type = "number" id = "charity" placeholder = "Charity Shine"><br>
                        <input type = "number" id = "health" placeholder = "Health Shine"><br>
                        <input type = "number" id = "social" placeholder = "Social Shine"><br>
-                       <button class = "btn" type = "button" id = "postbutton" onclick = "gethasurapushpost();"><img src = "css/post.png" width = "50px" height = "50px"></button>
+                       <button type = "button" id = "postbutton" onclick = "gethasurapushpost();"><img src = "css/post.png" width = "50px" height = "50px"></button>
                      </div>
                    </form>
                </div>
