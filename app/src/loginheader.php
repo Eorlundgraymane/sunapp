0d51c45d570b2b4ee02d124d3d40712f6d85372a
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 17:14:50 +0530

    Site Finished

--- app/src/loginheader.php
+++ app/src/loginheader.php
@@ -3,8 +3,8 @@
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "loginspace">
   <form method="POST" action = "/home.php" name = "loginform" id = "loginform">
-    <input type = "text" name = "primarykey" id = "primarykey" placeholder="Mobile">
-    <input type = "password" name = "password" id = "password" placeholder="Password">
+    <input required type = "text" name = "primarykey" id = "primarykey" placeholder="Mobile">
+    <input required type = "password" name = "password" id = "password" placeholder="Password">
     <button class = "btn" type = "button" id = "loginbutton" name = "login" onclick="userlogin();">Log in</button>
   </form>
 </div>
