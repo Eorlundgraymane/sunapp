8e97a11431e2c809019be884fce08b3fad95f9f2
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-19 21:51:56 +0530

    Debugs

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,15 +1,13 @@
 <row>
-  <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shinetoggles">
-  <button  title = "Shinetable" class = "btn" onclick = "refreshscore();" id = "shinetablebutton" type = "button" data-toggle = "collapse" data-target = "#shinetablediv">
-  <img width = "50px" height = "50px" src = "css/table.png">
-</button>
-<button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button">
-  <img width = "50px" height = "50px" src = "css/trophy.png">
-</button>
-</div>
-</row>
-<row>
-  <div class = "collapse col-lg-9 col-md-9 col-sm-9 col-xs-9" id = "shinetablediv">
+    <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "shinetoggles">
+    <button  title = "Shinetable" class = "btn" onclick = "refreshscore();" id = "shinetablebutton" type = "button" data-toggle = "collapse" data-target = "#shinetablediv">
+    <img width = "50px" height = "50px" src = "css/table.png">
+  </button>
+  <button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button">
+    <img width = "50px" height = "50px" src = "css/trophy.png">
+  </button>
+  </div>
+  <div class = "collapse col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "shinetablediv">
   <table id = "shinetable" class = "table table-responsive">
     <caption title = "Post activites,get likes, Improve your Shine"id = "shineheading">Shine Table</caption>
    <tr>
@@ -31,6 +29,6 @@
    <tr>
      <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Socialshine</th><th>Shine</th>
    </tr>
- </table>
-</div>
+  </table>
+  </div>
 </row>
