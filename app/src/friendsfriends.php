9ae7610bce189d89528039c7fa5fe5913eacb259
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-20 10:57:50 +0530

    More Green

--- app/src/friendsfriends.php
+++ app/src/friendsfriends.php
@@ -1,6 +1,6 @@
 <nav id = "col-xs-3 col-md-3 col-lg-3 col-sm-3 friendsbuttons">
     <button title = "Friends List" id = "friendslistbutton" onclick = "appfriendsfriendlogin();" type = "button" class = "btn" data-toggle = "collapse" data-target = "#friendslist">
-      <img src = "css/friendsicon.png" width = "50px" height = "50px">
+      <img src = "css/friendsicon.png" width = "50px" height = "40px">
     </button>
       <ul class = "collapse" id = "friendslist">
       </ul><br>
