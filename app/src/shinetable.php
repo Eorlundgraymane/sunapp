7afac48dcab121fdc37ecdd41c77263e3051b615
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-20 14:43:49 +0530

    More Green

--- app/src/shinetable.php
+++ app/src/shinetable.php
@@ -1,16 +1,16 @@
 <row>
   <div class = "col-lg-9 col-md-9 col-sm-9 col-xs-9">
-    <button  title = "Shinetable" class = "btn" onclick = "refreshscore();" id = "shinetablebutton" type = "button" data-toggle = "collapse" data-target = "#shinetable">
-  <img width = "50px" height = "50px" src = "css/table.png">
-</button>
 <button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button">
   <img width = "50px" height = "50px" src = "css/trophy.png">
 </button>
 </div>
 </row>
 <row>
-  <div id = "shinetable" class = "collapse col-lg-9 col-md-9 col-sm-9 col-xs-9">
+  <div id = "shinetable" class = "col-lg-9 col-md-9 col-sm-9 col-xs-9">
     <table class = "shinetable table table-responsive">
+      <button  title = "Shinetable" class = "btn" onclick = "refreshscore();" id = "shinetablebutton" type = "button">
+        <img width = "30px" height = "30px" src = "css/refreshpost.png">
+      </button>
       <caption title = "Post activites,get likes, Improve your Shine"id = "shineheading">Shine Table</caption>
      <tr>
        <td id = "friendshine">_</td>
