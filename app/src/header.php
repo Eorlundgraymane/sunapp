23b04121e66082199179890e9e7e6bc52f53bd92
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-20 15:14:37 +0530

    More Green

--- app/src/header.php
+++ app/src/header.php
@@ -2,9 +2,9 @@
   Sunshine</h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
-<button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button"><img width = "50px" height = "50px" src = "css/trophy.png"></button>
-<button title = "Home" id = "homebuttonbutton" class  = "btn sessionbutton" type = "button" onclick = "gopro();"><img width = "50px" height = "50px" src = "css/home.png"</button>
- <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "50px" height = "75px"</button>
+<button  title = "Leaderboards" class = "btn" onclick = "goleads();" id = "leaderboardsbutton" type = "button"><img width = "10%" src = "css/trophy.png"></button>
+<button title = "Home" id = "homebuttonbutton" class  = "btn sessionbutton" type = "button" onclick = "gopro();"><img width = "10%" src = "css/home.png"</button>
+ <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "10%" </button>
 </div>
 </div>
 </row>
