ef60024c03c3967f46db2800747c46a96a44c9ab
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-20 11:23:06 +0530

    More Green

--- app/src/friendtable.php
+++ app/src/friendtable.php
@@ -29,7 +29,7 @@
        <td><img id = "scoreshine" class = "shineicon img-responsive" src  = "css/generated/shine.svg" alt ="Family Icon"></td>
      </tr>
      <tr>
-       <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Socialshine</th><th>/Shine</th>
+       <th>Friendshine</th><th>Earthshine</th><th>Charityshine</th><th>Healthshine</th><th>Socialshine</th><th>Shine</th>
      </tr>
    </table>
   </div>
