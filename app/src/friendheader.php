fe73e5a91a9f9d99b44e9477154a9a76ef8b80e7
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-20 16:53:58 +0530

    UI Touch Ups

--- app/src/friendheader.php
+++ app/src/friendheader.php
@@ -1,4 +1,4 @@
-<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><h1 id = "siteh1">
+<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><img src = "css/slogo.jpg" width = "30px" height = "30px"><h1 id = "siteh1">
   Sunshine</h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
