bb5d5a51797848f44e2d4a8c78fd1218ae625841
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-26 11:45:00 +0530

    Added Hasura Link to Footer Power

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,3 +1,3 @@
 <row>
-    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Powered by <a href = "https://hasura.io">Hasura</a> | Made by PIRAVI 2.0</div>
+    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Powered by <a id = "hasuralink" href = "https://hasura.io">Hasura</a> | Made by PIRAVI 2.0</div>
 </row>
