430dfa529e15ca0b77b62d16eb56a8f19bd6fb72
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-29 15:51:26 +0530

    Scrollbar on Leaderboards

--- app/src/shineboardtables.php
+++ app/src/shineboardtables.php
@@ -1,4 +1,4 @@
-<div   class = "col-sm-12 col-xs-12 col-md-12 col-lg-12">
+<div id = "leadsdiv"  class = "col-sm-12 col-xs-12 col-md-12 col-lg-12">
   <table id = "leads" class = "table table-responsive shinetable">
   </table>
 </div>
