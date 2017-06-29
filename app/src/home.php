2b5792b9fdf65c428b1b3f037b3b759d74bb66e1
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-29 23:33:01 +0530

    Added Site Slogan on home page

--- app/src/home.php
+++ app/src/home.php
@@ -44,10 +44,10 @@
                        <textarea rows = "5" maxlength = "200" id = "newposttext" placeholder="Post here..."></textarea><br>
                        <div id = "allsmileys"></div>
                        <script>getallsmileys()</script>
-                       <input title = "Claim your Earthshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "earth" placeholder = "Earth Shine"><br>
-                       <input title = "Claim your Charityshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "charity" placeholder = "Charity Shine"><br>
-                       <input title = "Claim your Healthshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "health" placeholder = "Health Shine"><br>
-                       <input title = "Claim your Socialshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "social" placeholder = "Social Shine"><br>
+                       <input title = "Claim your Earthshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "earth" placeholder = "Earth Shine">Rate how much you cared for the nature<br>
+                       <input title = "Claim your Charityshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "charity" placeholder = "Charity Shine">Rate how much you showed charity<br>
+                       <input title = "Claim your Healthshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "health" placeholder = "Health Shine">Rate how much you took care of your body<br>
+                       <input title = "Claim your Socialshine points to go up in the leaderboards" class = "shineinput" type = "number" min = "0" max = "100" id = "social" placeholder = "Social Shine">Rate how much you fun you had with your family<br>
                        <button title = "Post" class = "btn" type = "button" id = "postbutton" onclick = "gethasurapushpost();"><img src = "css/post.png" width = "50px" height = "50px"></button>
                      </div>
                    </form>
