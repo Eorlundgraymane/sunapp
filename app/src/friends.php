5331ade3bc503694cbabf9cdfa3fec513df08489
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-29 16:19:07 +0530

    Scrollbar on Leaderboards

--- app/src/friends.php
+++ app/src/friends.php
@@ -2,13 +2,17 @@
     <button class = "btn" title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button"= data-toggle = "collapse" data-target = "#friendslist">
       <img src = "css/friendsicon.png" width = "50px" height = "40px">
     </button>
+    <div id = "friendslistdiv">
       <ul class = "collapse" id = "friendslist">
-      </ul><br>
+      </ul>
+    </div><br>
       <button class = "btn" title = "Friend Suggestions" id = "friendssuggestbutton" onclick = "applogin();" type = "button" data-toggle = "collapse" data-target = "#friendssuggest">
         <img width = "50px" height = "50px" src = "css/th.jpg">
       </button>
+      <div id = "friendssuggestdiv">
         <ul class = "collapse" id = "friendssuggest">
-        </ul><br>
+        </ul>
+      </div><br>
         <div id = "messages">
         <div class = "col-xs-3 col-md-3 col-lg-3 col-sm-3 sprite nosprite">
         </div><br><br><br><br><br><br><br><br>
