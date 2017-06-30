874d90e837f66c3c9d3e54739192df0a7db765b9
Eorlundgraymane <rkmenon235@gmail.com>
2017-07-01 00:10:47 +0530

    Added profile image on leaderboards

--- app/src/leaderboardtitle.php
+++ app/src/leaderboardtitle.php
@@ -1,6 +1,6 @@
 <div class = "col-sm-12 col-md-12 col-xs-12 col-lg-12">
   <figure>
-    <img class = "img img-responsive" id = "trophy" width = "100px" height="120px" src = "css/trophy.png">
+    <img title = "Shine Trophy" class = "img img-responsive" id = "trophy" width = "100px" height="120px" src = "css/trophy.png">
     <div id = "lead">Leaderboard</div>
   </figure>
 </div>
