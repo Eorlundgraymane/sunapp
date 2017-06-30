b6e22f828fe83f7de9af06bb4bdb580777acebdf
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-30 19:57:01 +0530

    Delay of refresh post increased to reduce CPU load on client

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -2688,12 +2688,12 @@ function loadtable(id){
     xhr.send(query);
 }
 function keeppulling(){
-  console.log("Will refresh posts again in a minute");
-  setTimeout(function(){console.log("pulling posts again");gethasurapullpost();},60000);
+  console.log("Will refresh posts again in a couple of minutes");
+  setTimeout(function(){console.log("pulling posts again");gethasurapullpost();},2*60*1000);
 }
 function keepfpulling(){
-  console.log("Will refresh posts again in a minute");
-  setTimeout(function(){console.log("pulling posts again");pullfposts(parseInt(getCookie("friendid")));},60000);
+  console.log("Will refresh posts again in a couple of minutes");
+  setTimeout(function(){console.log("pulling posts again");pullfposts(parseInt(getCookie("friendid")));},2*60*1000);
 }
 function pullfposts(id){
   document.getElementById('postpuller').innerHTML = "Refreshing Posts <img src = css/loader.gif width = \"30px\" height = \"30px\">";
