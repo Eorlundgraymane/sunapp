15fceb2b2f6894226d0c7e192a5a37131a73e24c
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-30 16:17:10 +0530

    Reduced Console Logs

--- app/src/friendprofile.php
+++ app/src/friendprofile.php
@@ -29,7 +29,7 @@
                <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "friendspane">
                  <?php include 'friendsfriends.php';?>
                </div>
-               <div onload = "keepfpulling();" class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
+               <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
                      <button class = "btn" id = "postpuller"  type = "button" onclick = "pullfposts(parseInt(getCookie('friendid')));" ><img id = "postrefresh" src = "css/refreshpost.png" width = "50px" height = "50px"></button>
                      <ul id = "posts">
 
