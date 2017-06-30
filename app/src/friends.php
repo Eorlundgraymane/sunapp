944091dbdea67b21e4d42ae5f3f695486e853e03
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-30 16:50:06 +0530

    Styling

--- app/src/friends.php
+++ app/src/friends.php
@@ -1,13 +1,13 @@
 <nav id = "col-xs-3 col-md-3 col-lg-3 col-sm-3 friendsbuttons">
     <button class = "btn" title = "Friends List" id = "friendslistbutton" onclick = "appfriendslogin();" type = "button"= data-toggle = "collapse" data-target = "#friendslist">
-      <img src = "css/friendsicon.png" width = "50px" height = "40px">
+      <img class = "img-rounded" src = "css/friendsicon.png" width = "50px" height = "40px">
     </button>
     <div id = "friendslistdiv">
       <ul class = "collapse" id = "friendslist">
       </ul>
     </div><br>
       <button class = "btn" title = "Friend Suggestions" id = "friendssuggestbutton" onclick = "applogin();" type = "button" data-toggle = "collapse" data-target = "#friendssuggest">
-        <img width = "50px" height = "50px" src = "css/th.jpg">
+        <img class = "img-rounded" width = "50px" height = "50px" src = "css/th.jpg">
       </button>
       <div id = "friendssuggestdiv">
         <ul class = "collapse" id = "friendssuggest">
