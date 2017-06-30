b8e65e32e289771537b0f5a3dcace6b605a64140
Eorlundgraymane <rkmenon235@gmail.com>
2017-06-30 18:04:20 +0530

    Styling

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,3 +1,3 @@
 <row>
-    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Powered by <a id = "hasuralink" target = "_blank" href = "https://hasura.io">Hasura</a> | Made by <a href = "mailto:rkmenon235@gmail.com">PIRAVI 2.0</a></div>
+    <div class = "col-lg-12 col-md-12 col-sm-12 col-xs-12" id = "sitefooter">Powered by <a id = "hasuralink" target = "_blank" href = "https://hasura.io">Hasura</a> | Made by <a id = "hasuralink" target = "_blank" href = "mailto:rkmenon235@gmail.com">PIRAVI 2.0</a></div>
 </row>
