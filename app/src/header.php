7342d55359f70ba6475950a00f3fff84c38fccd5
Eorlundgraymane <rkmenon235@gmail.com>
2017-07-06 09:13:42 +0530

    Default Profile Picture

--- app/src/header.php
+++ app/src/header.php
@@ -1,5 +1,5 @@
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><h1 id = "siteh1">
-  <a id = "sunlink"href = "https://sunshine.unwound15.hasura-app.ioSunshine"</a></h1>
+  <a id = "sunlink"href = "https://sunshine.unwound15.hasura-app.ioSunshine">Sunshine</a></h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
   <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "30px" height = "30px"></button>
