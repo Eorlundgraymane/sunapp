0fb183706879bc9af9101a9f9dbf7cc8028e199d
Eorlundgraymane <rkmenon235@gmail.com>
2017-07-06 08:31:52 +0530

    Default Profile Picture

--- app/src/home.php
+++ app/src/home.php
@@ -30,7 +30,7 @@
                  <?php include 'friends.php';?>
                </div>
                <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "newspane">
-                     <button title = "Posts will refresh every 30s" class = "btn" id = "postpuller" type = "button" onclick = "gethasurapullpost();" ><img id = "postrefresh" src = "css/refreshpost.png" width = "30px" height = "30px"></button>
+                     <button title = "Posts will refresh every 5 minutes" class = "btn" id = "postpuller" type = "button" onclick = "gethasurapullpost();" ><img id = "postrefresh" src = "css/refreshpost.png" width = "30px" height = "30px"></button>
                      <ul id = "posts">
 
                      </ul>
