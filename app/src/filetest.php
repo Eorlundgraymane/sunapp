406e27fe88576c70f2efe224f516db6fe4016492
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-08 20:45:44 +0530

    unwound15 to animation75

--- app/src/filetest.php
+++ app/src/filetest.php
@@ -10,6 +10,7 @@
     <input type="file" name="fileToUpload" id="ftu">
     <button onclick = "fileup(getCookie('hasura_id'))" type="button" value="Upload Image">Upload Image</button><br><br>
 
+    <button onclick = "filedown(getCookie('hasura_id'))" type = "button" value = "Check Image">Check Image</button><br><br>
     <img width = "640px" heigh = "320px" id = "img" alt = "No Image Uploaded"><br><br>
 
     <button onclick = "filedel(getCookie('hasura_id'))" type="button" value="Delete Image">Delete Image</button><br><br>
--- app/src/header.php
+++ app/src/header.php
@@ -2,10 +2,10 @@
   <a title = "Sunshine" id = "sunlink"href = "https://sunshine.unwound15.hasura-app.io/">Sunshine</a></h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
-  <button title = "Uploader" id = "uploadadvert" class = "btn sessionbutton" type = "button" onclick = "window.location = 'https://sunshine.animation75.hasura-app.io/filetest.php'"><img src = "/css/upload.jpg" width = "30px" height = "30px"></button>
   <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "30px" height = "30px"></button>
   <button title = "Home" id = "homebuttonbutton" class  = "btn sessionbutton" type = "button" onclick = "gopro();"><img width = "30px" height = "30px" src = "css/home.png"></button>
   <button  title = "Leaderboard" class = "btn sessionbutton" onclick = "goleads();" id = "leaderboardsbutton" type = "button"><img width = "30px" height = "30px" src = "css/trophy.png"></button>
+  <button title = "Test out our new Uploader" id = "uploadadvert" class = "btn sessionbutton" type = "button" onclick = "window.location = 'https://sunshine.animation75.hasura-app.io/filetest.php'"><img src = "/css/upload.jpg" width = "30px" height = "30px"></button>
 </div>
 </div>
 </row>
