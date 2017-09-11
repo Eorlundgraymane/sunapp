c60b3bbdddf2376bee74b25ff392fa5704967b3f
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-11 19:25:49 +0530

    UI Tweaks

---
+++ app/src/myimages.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include 'headprereqs.php';?>
+    <script>
+    if(checkCookie("primarykey")){
+      gopro();
+    }
+    </script>
+    <title>Sunshine | My Images</title>
+  </heaad
