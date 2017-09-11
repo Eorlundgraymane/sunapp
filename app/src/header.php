61d1b0e07c1b0cee481e1b664cafbcdc611dfab0
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-11 11:24:03 +0530

    Bugfix

--- app/src/header.php
+++ app/src/header.php
@@ -2,6 +2,7 @@
   <a title = "Sunshine" id = "sunlink"href = "https://sunshine.animation75.hasura-app.io/">Sunshine</a></h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
+  <button title = "Notifications and Messages coming soon" id = "notifbutton" class = "btn sessionbutton" type = "button" onclick = "alert('hang on we're working on it :-\')"><img width = "30px" height ="30px" src = "/css/notif.jpg"></button>
   <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "30px" height = "30px"></button>
   <button title = "Home" id = "homebuttonbutton" class  = "btn sessionbutton" type = "button" onclick = "gopro();"><img width = "30px" height = "30px" src = "css/home.png"></button>
   <button  title = "Leaderboard" class = "btn sessionbutton" onclick = "goleads();" id = "leaderboardsbutton" type = "button"><img width = "30px" height = "30px" src = "css/trophy.png"></button>
