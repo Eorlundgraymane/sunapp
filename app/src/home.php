22542e3cb349f6eae23d5a09baf526587b350b71
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-11 17:52:26 +0530

    UI Tweaks

--- app/src/home.php
+++ app/src/home.php
@@ -43,13 +43,15 @@
                <div class = "col-lg-3 col-md-3 col-sm-3 col-xs-3" id = "postpane">
                  <h3 title = "Share a photo of your dedication to being more human and get likes!!!" id = "uploadpane">Upload New Post</h3>
                    <form id  = "postform" method = "POST" action = "#">
-                     <div id = "uploadform" class = "form-group">                       
-                       <label class = "radio-inline">
+                     <div id = "uploadform" class = "form-group">
+                       <div class = "radio-inline">
+                       <label>
                          <input type = "radio" id = "imgradio"><button onclick = "addimg();" type = "button" class = "btn" title = "Add Image to your Post"><img src = "/css/imgpost.jpg"></button></input>
                        </label>
-                       <label class = "radio-inline">
+                       <label>
                          <input type = "radio" id = "vidradio"><button onclick = "addvid();" type = "button" class = "btn" title = "Add Video to your Post"><img src = "/css/vidpost.jpg"></button></input>
-                       </label><br>
+                       </label>
+                     </div><br>
                        <input  type = "text" id = "newposttitle" placeholder = "Title"><br>
                        <input  type = "text" id = "newpostimg" placeholder = "Image URL"><br>
                        <textarea rows = "5" maxlength = "200" id = "newposttext" placeholder="Post here..."></textarea><br>
