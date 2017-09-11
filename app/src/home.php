ab0c6f06a1d5c578db324e1231cafb30a6733932
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-11 18:12:44 +0530

    UI Tweaks

--- app/src/home.php
+++ app/src/home.php
@@ -47,10 +47,12 @@
                       <button onclick = "addimg();" type = "button" class = "btn" title = "Add Image to your Post"><img width = "30px" height = "30px" src = "/css/imgpost.jpg"></button><button onclick = "addvid();" type = "button" class = "btn" title = "Add Video to your Post"><img width = "30px" height = "30px" src = "/css/vidpost.jpg"></button>
                      </div><br>
                       <div id = "imageuploader" class = "hidden">
-                       <input type = "file" id = "imgup" name = "Upload Image">
+                        <label for = "imgup">Add Image</label>
+                       <input type = "file" id = "imgup" visibility = "hidden">
                      </div>
                      <div id = "videouploader" class = "hidden">
-                      <input type = "file" id = "vidup" name = "Upload Image">
+                       <label for = "vidup">Add Video Clip</label>
+                      <input type = "file" id = "vidup" visibility = "hidden" >
                     </div>
                        <input  type = "text" id = "newposttitle" placeholder = "Title"><br>
                        <input  type = "text" id = "newpostimg" placeholder = "Image URL"><br>
