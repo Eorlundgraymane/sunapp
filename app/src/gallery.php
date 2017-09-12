9e1ea819ac0579892a2a03e2f0a68a1daee5f58c
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-12 19:54:35 +0530

    Brightening

---
+++ app/src/gallery.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sunshine | Gallery</title>
+    <?php include 'headprereqs.php';?>
+    <?php include 'loginchecker.php';?>
+  </head>
+  <body class = "outer-container">
+    
+
+  </body>
+</html>
