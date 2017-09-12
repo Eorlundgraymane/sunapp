9300d555a90b196d133893625c062af912690da6
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-12 20:32:14 +0530

    Brightening

--- app/src/friendheader.php
+++ app/src/friendheader.php
@@ -1,4 +1,4 @@
-<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><a title = "Sunshine" id = "sunlink"href = "https://sunshine.unwound15.hasura-app.io/">Sunshine</a></h1>
+<div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sitename"><a title = "Sunshine" id = "sunlink"href = "https://sunshine.animation75.hasura-app.io/">Sunshine</a></h1>
 </div>
 <div class = "col-lg-6 col-md-6 col-sm-6 col-xs-6" id = "sessionbutton">
   <button title = "Log Out" id = "logoutbutton" class  = "btn sessionbutton" type = "button" onclick = "userlogout(getCookie('primarykey'));"><img src = "css/logout.png" width = "30px" height = "30px"></button>
