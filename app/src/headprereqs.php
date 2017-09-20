89cbc436c350b0bc764b857c59185eba72d16d62
Eorlundgraymane <rkmenon235@gmail.com>
2017-09-20 22:59:22 +0530

    Google Site Verification

--- app/src/headprereqs.php
+++ app/src/headprereqs.php
@@ -12,5 +12,6 @@
 <meta name = "viewport" content = "width=device-width,initial-scale=1">
 <meta name = "description" content = "Sunshine is a website which gives you the motivation to Be More Human Make, Friends, Help the needy,Plant a tree or feed some birds regularly,Go to the Gym, Spend some time with your family,Post and Share your Activities, Get Likes, Shine brighter on the leaderboards">
 <meta name = "author" content = "PIRAVI 2.0">
+<meta name="google-site-verification" content="d09aG7NJPLSfMWk5kN4B6_KrogtkxjENCqLyVIt2AfQ" />
 <meta name = "keywords" content = "Human,Social,Media,Share,Like,Post,Help,Save,Trees,Animals,Plants,Charity,Quality,Time,Celebrate,Exercise,Fun,Leaderboards">
 <link rel="icon" href="css/favicon.ico" sizes="16x16">
